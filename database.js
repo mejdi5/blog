3f5f62c7569ff5c28a0b79075e9bc9d7f545a188
Mejdi Ben Ammou <mejdi.ben.ammou@gmail.com>
2022-10-11 16:04:00 +0200

    deploy

--- database.js
+++ database.js
@@ -1,9 +1,10 @@
 import mysql from 'mysql'
 
 export const db = mysql.createConnection({
-    host:"localhost",
-    user:"root",
-    password: "abcdefgh",
-    database: "blog",
+    host: process.env.HOST || "localhost",
+    user: process.env.USER || "root",
+    password: process.env.PASSWORD || "abcdefgh",
+    database: process.env.DATABASE || "blog",
+    post: process.env.PORT || 3306
 })
 
--- server.js
+++ server.js
@@ -22,6 +22,6 @@ db.connect((err) => {
 app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
 
-app.listen(process.env.PORT || 5000, () => {
+app.listen(5000, () => {
   console.log("server running..");
 });
